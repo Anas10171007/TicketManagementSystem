95769ffee4de739a1990df88b0b16f223f7ee1a4
MuhammadAnas-25i3062 <i253062@isb.nu.edu.pk>
2026-07-13 17:26:58 +0500

    JWT authentication in Swagger and DB.

--- FIRSTPROJECT.Application/Authentication/Validators/RegisterDtoValidator.cs
+++ FIRSTPROJECT.Application/Authentication/Validators/RegisterDtoValidator.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using FIRSTPROJECT.Application.Authentication.DTOs;
+using FluentValidation;
 
-namespace FIRSTPROJECT.Application.Authentication.Validators
+namespace FIRSTPROJECT.Application.Authentication.Validators;
+
+public class RegisterDtoValidator : AbstractValidator<RegisterDto>
 {
-    internal class RegisterDtoValidator
+    public RegisterDtoValidator()
     {
+        RuleFor(x => x.FullName)
+            .NotEmpty();
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(6);
     }
 }
